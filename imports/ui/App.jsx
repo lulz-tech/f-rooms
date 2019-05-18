e66cec89853cbd9f37a70544abdce5417bcbb840
Alex Mubarakshin <muban96@mail.ru>
2019-05-18 15:26:51 +0300

    ☝️ Minor update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import Hello from './components/Hello.jsx';
+import Info from './components/Info.jsx';
 
 const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
+    <div>
+        <h1>F-Rooms</h1>
+        <Hello />
+        <Info />
+    </div>
 );
 
 export default App;
